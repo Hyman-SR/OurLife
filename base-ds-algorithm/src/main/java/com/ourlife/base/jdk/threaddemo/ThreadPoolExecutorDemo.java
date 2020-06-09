6f16086f60fff30a97085f4cefadb859be151809
张超 <zhangchao4409@cvte.com>
2020-06-09 08:54:41 +0800

    [practice] 练习jdk相关知识

--- base-ds-algorithm/src/main/java/com/ourlife/base/jdk/threaddemo/ThreadPoolExecutorDemo.java
+++ base-ds-algorithm/src/main/java/com/ourlife/base/jdk/threaddemo/ThreadPoolExecutorDemo.java
@@ -1,8 +1,17 @@
 package com.ourlife.base.jdk.threaddemo;
 
-import java.util.concurrent.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
+ * 正式在项目中使用，避免直接使用JDK提供的以下四种线程池，因为默认参数会存在很多问题：https://www.cnblogs.com/gaopengpy/p/12148971.html
+ * 1.newFixedThreadPool，内部的等待队列默认为Integer.MAX，可能会堆积非常多的请求，存在OOM的风险
+ * 2.newSingleThreadExecutor，内部的等待队列默认为Integer.MAX，可能会堆积非常多的请求，存在OOM的风险
+ * 3.newCachedThreadPool，内部的最大线程数是Integer.MAX，可能会创建非常多的线程，存在OOM的风险
+ * 4.newScheduledThreadPool，内部的最大线程数是Integer.MAX，可能会创建非常多的线程，存在OOM的风险
+ *
  * 线程池逻辑
  * 1.在创建了线程池后，等待提交过来的任务请求
  * 2.在调用execute()方法添加一个请求任务时，线程池会做如下判断：
@@ -11,7 +20,15 @@ import java.util.concurrent.*;
  *  2.3 如果这个时候队列满了且正在运行的线程数量还小于maximumPoolSize，那么还是要创建非核心线程立刻运行这个任务
  *  2.4 如果队列满了且正在运行的线程数量大于或等于maximumPoolSize，那么线程池会启动饱和和拒绝策略来执行
  * 3.当一个线程完成任务时，它会从队列中取下一个任务来执行
- * 4.当一个线程无事可做超过一定时间keepAliveTime时，线程池会判断：如果当前运行的线程数大于corePoolSize，那么这个线程会被停掉
+ * 4.当一个线程无事可做超过一定时间keepAliveTime时，线程池会判断：
+ *  如果当前运行的线程数大于corePoolSize，那么这个线程会被停掉
+ *  所以线程池的所有任务完成后它最终会收缩到corePoolSize的大小
+ *
+ *  JDK内置的拒绝策略(生产环境避免使用AbortPolicy，线程超卖后，让你怀疑人生，建议使用第二种：CallerRunsPolicy)：
+ *  1.AbortPolicy(默认)：直接抛出RejectedExecutionException异常阻止系统正常运行
+ *  2.CallerRunsPolicy："调度者运行"一种调节机制，该策略既不会抛弃任务，也不会抛出异常，而是将某些任务回退到调用者，从而降低新任务的流量
+ *  3.DiscardOldestPolicy：抛弃队列中等待最久的任务，不抛出异常
+ *  4.DiscardPolicy：直接丢弃任务，不予任何处理，也不抛出异常
  *
  * @author zhangchao
  * @createdOn 2020/6/8
@@ -19,21 +36,39 @@ import java.util.concurrent.*;
 public class ThreadPoolExecutorDemo {
 
     public static void main(String[] args) {
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2,
+
+//        // TODO 都包括哪些线程池类型？  https://www.cnblogs.com/gaopengpy/p/12148971.html
+//        //内部的等待队列默认为Integer.MAX，可能会堆积非常多的请求，存在OOM的风险
+//        Executors.newFixedThreadPool(10);
+//        //内部的等待队列默认为Integer.MAX，可能会堆积非常多的请求，存在OOM的风险
+//        Executors.newSingleThreadExecutor();
+//        //内部的最大线程数是Integer.MAX，可能会创建非常多的线程，存在OOM的风险
+//        Executors.newCachedThreadPool();
+//        //内部的最大线程数是Integer.MAX，可能会创建非常多的线程，存在OOM的风险
+//        Executors.newScheduledThreadPool(10);
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                2,
                 5,
                 100L,
                 TimeUnit.SECONDS,
                 new LinkedBlockingDeque<>(3),
                 Executors.defaultThreadFactory(),
-                new ThreadPoolExecutor.AbortPolicy());
-
+                //1.AbortPolicy 超出线程池处理能力，直接抛异常RejectedExecutionException
+//                new ThreadPoolExecutor.AbortPolicy());
+                //2.CallerRunsPolicy 超出线程池处理能力，就将任务退还给任务调度线程，即main线程
+//                new ThreadPoolExecutor.CallerRunsPolicy());
+                //3.会丢弃阻塞队列中最老的任务
+                new ThreadPoolExecutor.DiscardOldestPolicy());
+                //4.会直接丢弃阻塞队列中的任务
+//                new ThreadPoolExecutor.DiscardPolicy());
         try {
-            for (int i = 0; i < 6; i++) {
+            for (int i = 0; i < 1000; i++) {
                 final int temp = i + 1;
                 threadPoolExecutor.execute(() -> {
                     System.out.println(Thread.currentThread().getName() + "号窗口，服务顾客" + temp);
                     try {
-                        TimeUnit.SECONDS.sleep(4);
+                        TimeUnit.SECONDS.sleep(1);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
